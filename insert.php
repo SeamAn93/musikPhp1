901463db832e085fc0a9fb16cdd82acd2458cf17
David <david.osbeck1996@gmail.com>
2021-09-15 09:42:57 +0200

    Gjort formular responsiv til desktop

--- insert.php
+++ insert.php
@@ -26,56 +26,61 @@ if(!empty($_POST["data"])){
 <body class="bg-background">
 
 <section class="main container">
-<div class="header d-flex justify-content-between mt-5 mb-4 mb-md-5 text-white">
-    <h1 class="header-title">Music Database</h1>
-    <button class="btn btn-primær text-white btn-lg"><i class="fas fa-home"></i> Back to home</button>
-</div>
+    <div class="header d-flex justify-content-between mt-5 mb-4 mb-md-5 text-white">
+        <h1 class="header-title">Music Database</h1>
+        <button class="btn btn-primær text-white btn-lg"><i class="fas fa-home"></i> Back to home</button>
+    </div>
+
+    <div class="header">
+        <h2 class="header__title text-white mb-5 pt-4 fw-normal">Add a new song/artist/album...</h2>
+    </div>
 
 <form method="post" action="insert.php">
     <div class="row">
-        <div class="col-12 col-md-7 mb-2">
+        <div class="col-12 col-md-6 mb-2">
             <div class="mb-3 text-white">
                 <label for="musikTitel" class="form-label fs-5">Title</label>
                 <input class="form-control" type="text" name="data[musikTitel]" id="musikTitel" placeholder="Title" value="">
             </div>
         </div>
 
-        <div class="col-12 col-md-7 mb-2">
+        <div class="col-12 col-md-6 mb-2">
             <div class="mb-3 text-white">
                 <label for="musikArtist" class="form-label fs-5">Artist</label>
                 <input class="form-control" type="text" name="data[musikArtist]" id="musikArtist" placeholder="Artist" value="">
             </div>
         </div>
 
-        <div class="col-12 col-md-7 mb-2">
+        <div class="col-12 col-md-6 mb-2">
             <div class="mb-3 text-white">
                 <label for="musikAlbum" class="form-label fs-5">Album</label>
                 <input class="form-control" type="text" name="data[musikAlbum]" id="musikAlbum" placeholder="Album" value="">
             </div>
         </div>
 
-        <div class="col-12 col-md-7 mb-2">
+        <div class="col-12 col-md-6 mb-2">
             <div class="mb-3 text-white">
-                <label for="musikGenre" class="form-label fs-5">Genre</label>
-                <input class="form-control" type="text" name="data[musikGerne]" id="musikGenre" placeholder="Genre" value="">
+                <label for="musikGenre" class="form-label fs-5">Genre / subgenre</label>
+                <input class="form-control" type="text" name="data[musikGerne]" id="musikGenre" placeholder="Genre / subgenre" value="">
             </div>
         </div>
 
-        <div class="col-12 col-md-7 mb-2">
+        <div class="col-12 col-md-4 mb-2">
             <div class="mb-3 text-white">
-                <label for="musikOrigin" class="form-label fs-5">Origin</label>
+                <label for="musikOrigin" class="form-label fs-5">Where is the artist from?</label>
                 <input class="form-control" type="text" name="data[musikOrigin]" id="musikOrigin" placeholder="Origin" value="">
             </div>
         </div>
 
-        <div class="col-6 mb-2">
+
+        <div class="col-4 mb-2">
             <div class="mb-3 text-white">
                 <label for="musikRelease" class="form-label fs-5">Date of release</label>
                 <input class="form-control" type="date" name="data[musikRelease]" id="musikRelease" placeholder="" value="">
             </div>
         </div>
 
-        <div class="col-6 mb-2">
+        <div class="col-4 mb-2">
             <div class="mb-3 text-white">
                 <label for="musikTime" class="form-label fs-5">Track length</label>
                 <input class="form-control" type="text" name="data[musikTime]" id="musikTime" placeholder="00.00" value="">
@@ -98,14 +103,14 @@ if(!empty($_POST["data"])){
 
         <div class="col-12 col-md-6 mb-2">
             <div class="mb-3 text-white">
-                <label for="musikBuy" class="form-label fs-5">iTunes link</label>
-                <input class="form-control" type="text" name="data[musikBuy]" id="musikBuy" placeholder="iTunes" value="">
+                <label for="musikBuy" class="form-label fs-5">Link to iTunes</label>
+                <input class="form-control" type="text" name="data[musikBuy]" id="musikBuy" placeholder="Link to iTunes" value="">
             </div>
         </div>
 
         <div class="btn-container d-flex justify-content-center my-4">
             <a href="index.php"><button class="btn btn-cancel btn-lg text-white mx-3">Cancel</button></a>
-            <button class="btn btn-primær btn-lg text-white mx-3" type="submit" id="btnSubmit">Confirm</button>
+            <button class="btn btn-primær btn-lg text-white mx-3" type="submit" id="btnSubmit">Confirm</i></button>
         </div>
 
 </section>
