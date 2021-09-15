340003d226031b59e17b472bfda8e6fef2ba0cfe
David <david.osbeck1996@gmail.com>
2021-09-15 11:52:30 +0200

    Tilføjet pris og amazon link input

--- insert.php
+++ insert.php
@@ -1,12 +1,14 @@
 <?php
 require "settings/init.php";
+
 if(!empty($_POST["data"])){
     $data = $_POST["data"];
     $sql = "INSERT INTO information (musikTitel, musikArtist, musikAlbum, musikGerne, musikOrigin, musikRelease, musikTime, musikBandInstrument, musikAlbumTracklist, musikBuy) VALUES(:musikTitel, :musikArtist, :musikAlbum, :musikGerne, :musikOrigin, :musikRelease, :musikTime, :musikBandInstrument, :musikAlbumTracklist, :musikBuy)";
     $bind = [":musikTitel" => $data["musikTitel"],":musikArtist" => $data["musikArtist"],":musikAlbum" => $data["musikAlbum"],":musikGerne" => $data["musikGerne"],":musikOrigin" => $data["musikOrigin"],":musikRelease" => $data["musikRelease"], ":musikTime" => $data["musikTime"],":musikBandInstrument" => $data["musikBandInstrument"],":musikAlbumTracklist" => $data["musikAlbumTracklist"],":musikBuy" => $data["musikBuy"]];
     $db->sql($sql,$bind,false);
-    echo "Produktet er nu indsat. <a href='insert.php'>Indsæt er produkt mere</a>";
-    exit;
+
+    header('Location: insert.php?insert');
+
 }
 ?>
 <!DOCTYPE html>
@@ -23,12 +25,12 @@ if(!empty($_POST["data"])){
     <script src="https://cdn.tiny.cloud/1/hlgnu720dcrihycfqzdw381s3lo7v9n7snkn1j9t1yizavot/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script src="https://kit.fontawesome.com/d7a71e7a7e.js" crossorigin="anonymous"></script>
 </head>
-<body class="bg-background">
+<body>
 
 <section class="main container">
     <div class="header d-flex justify-content-between mt-5 mb-4 mb-md-5 text-white">
         <h1 class="header-title">Music Database</h1>
-        <button class="btn btn-primær text-white btn-lg"><i class="fas fa-home"></i> Back to home</button>
+        <button class="btn btn-primary text-white btn-lg"><i class="fas fa-home"></i> Back to home</button>
     </div>
 
     <div class="header">
@@ -38,16 +40,16 @@ if(!empty($_POST["data"])){
 <form method="post" action="insert.php">
     <div class="row">
         <div class="col-12 col-md-6 mb-2">
-            <div class="mb-3 text-white">
+            <div class=" mb-3 text-white">
                 <label for="musikTitel" class="form-label fs-5">Title</label>
-                <input class="form-control" type="text" name="data[musikTitel]" id="musikTitel" placeholder="Title" value="">
+                    <input class="form-control" type="text" name="data[musikTitel]" id="musikTitel" placeholder="Title" value="" required>
             </div>
         </div>
 
         <div class="col-12 col-md-6 mb-2">
-            <div class="mb-3 text-white">
+            <div class="has-validation mb-3 text-white">
                 <label for="musikArtist" class="form-label fs-5">Artist</label>
-                <input class="form-control" type="text" name="data[musikArtist]" id="musikArtist" placeholder="Artist" value="">
+                <input class="form-control" type="text" name="data[musikArtist]" id="musikArtist" placeholder="Artist" value="" required>
             </div>
         </div>
 
@@ -74,50 +76,91 @@ if(!empty($_POST["data"])){
 
 
         <div class="col-4 mb-2">
-            <div class="mb-3 text-white">
+            <div class="has-validation mb-3 text-white">
                 <label for="musikRelease" class="form-label fs-5">Date of release</label>
-                <input class="form-control" type="date" name="data[musikRelease]" id="musikRelease" placeholder="" value="">
+                <input class="form-control" type="date" name="data[musikRelease]" id="musikRelease" placeholder="" value="" required>
             </div>
         </div>
 
         <div class="col-4 mb-2">
-            <div class="mb-3 text-white">
+            <div class="has-validation mb-3 text-white">
                 <label for="musikTime" class="form-label fs-5">Track length</label>
-                <input class="form-control" type="text" name="data[musikTime]" id="musikTime" placeholder="00.00" value="">
+                <input class="form-control" type="time" name="data[musikTime]" id="musikTime" step="1" placeholder="00:00:00" value="00:00:00" min="00:00:00" required>
             </div>
         </div>
 
         <div class="col-12">
             <div class="mb-3 text-white">
                 <label for="musikBandInstrument" class="form-label fs-5">Band members / instruments</label>
-                <textarea class="form-control" name="data[musikBandInstrument]" id="musikBandInstrument"></textarea>
+                <textarea class="form-control" name="data[musikBandInstrument]" id="musikBandInstrument" placeholder="John doe - Guitar"></textarea>
             </div>
         </div>
 
         <div class="col-12">
             <div class="mb-3 text-white">
                 <label for="musikAlbumTracklist" class="form-label fs-5">Album tracklist</label>
-                <textarea class="form-control" name="data[musikAlbumTracklist]" id="musikAlbumTracklist"></textarea>
+                <textarea class="form-control" name="data[musikAlbumTracklist]" id="musikAlbumTracklist" placeholder="Track #1 - 00:00"></textarea>
             </div>
         </div>
 
         <div class="col-12 col-md-6 mb-2">
             <div class="mb-3 text-white">
-                <label for="musikBuy" class="form-label fs-5">Link to iTunes</label>
-                <input class="form-control" type="text" name="data[musikBuy]" id="musikBuy" placeholder="Link to iTunes" value="">
+                <label for="musikBuy" class="form-label fs-5">Amazon product link</label>
+                <input class="form-control" type="text" name="data[musikBuy]" id="musikBuy" placeholder="Amazon product link" value="">
+            </div>
+        </div>
+
+        <div class="col-12 col-md-2 mb-2">
+            <div class="mb-3 text-white">
+                <label for="musikPrice" class="form-label fs-5">Price</label>
+                <input class="form-control" type="text" name="data[musikPrice]" id="musikPrice" placeholder="$" value="">
             </div>
         </div>
 
         <div class="btn-container d-flex justify-content-center my-4">
             <a href="index.php"><button class="btn btn-cancel btn-lg text-white mx-3">Cancel</button></a>
-            <button class="btn btn-primær btn-lg text-white mx-3" type="submit" id="btnSubmit">Confirm</i></button>
+            <button class="btn btn-primary btn-lg text-white mx-3" type="submit" id="btnSubmit">Confirm</i></button>
         </div>
 
 </section>
+
+<?php
+if(isset($_GET["insert"])){
+    ?>
+    <div class="modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <p>Your song/artist/album has been added, want to add another?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-lg btn-cancel">Back to home</button>
+                    <a href="insert.php" class="btn btn-lg btn-primary text-white">Confirm</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+}
+?>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     tinymce.init({
         selector: 'textarea',
     });
+
+
+    const modal = document.querySelector('.modal');
+    if(modal){
+        const bsModal = new bootstrap.Modal(modal, {keyboard: false});
+        bsModal.show();
+
+        modal.addEventListener('hide.bs.modal', () => {
+            document.location = "index.php";
+        })
+    }
+
 </script>
 </body>
 </html>
