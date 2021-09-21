95b1dfb42d1149f0ace8d48158a6245316995403
ander <soer9428@edu.easj.dk>
2021-09-21 14:23:21 +0200

    - Opdatering af sql database - Opsætning af php side til visning af tracks - Tjek adobe XD for muckups

---
+++ track.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    <title>Musik Database</title>
+    <meta name="robots" content="All">
+    <meta name="author" content="David Osbeck & Søren Andersen">
+    <meta name="copyright" content="David Osbeck & Søren Andersen">
+    <link href="css/styles.scss" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.tiny.cloud/1/hlgnu720dcrihycfqzdw381s3lo7v9n7snkn1j9t1yizavot/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://kit.fontawesome.com/d7a71e7a7e.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
